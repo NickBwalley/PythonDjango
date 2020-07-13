b60f9a70ff2ef91052c95eb0ccb0770ba778a4a3
NickBwalley <nickbwalley@gmail.com>
2020-07-13 14:10:13 +0300

    Django

--- WebProject/WebApp/views.py
+++ WebProject/WebApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hi(request):
-    return HttpResponse('<h1> DJANGO FIRST HOMEPAGE!..</h1>')
+    return render(request, 'WebApp/helloWorld.html')

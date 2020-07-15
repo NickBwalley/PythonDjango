bfb08eaff04c3b3b09c0f88a688f2ba3a513dd3b
NickBwalley <nickbwalley@gmail.com>
2020-07-15 12:02:58 +0300

    Django

--- Orphanage/Nicky/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NickyConfig(AppConfig):
-    name = 'Nicky'
--- Orphanage/Nicky/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def hi(request):
-    return render(request,'Nicky/homepage.html')
-def hello(request):
-    return render(request,'Nicky/nicky1.css')

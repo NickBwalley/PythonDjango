22a91f618e248f7628bfb584841a99abb3f8631e
NickBwalley <nickbwalley@gmail.com>
2021-04-26 21:14:21 +0300

    DjangoProjects

--- mrbeastproject/mrbeastproject/settings.py
+++ mrbeastproject/mrbeastproject/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mrbeastproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

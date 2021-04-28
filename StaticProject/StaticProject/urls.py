2c785a0ac1dcfa4c3a1008a5c36f562b6d3c6937
NickBwalley <nickbwalley@gmail.com>
2021-04-28 09:49:56 +0300

    Django StaticFiles

--- StaticProject/StaticProject/urls.py
+++ StaticProject/StaticProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('App1.urls')),
+    path('', include('App2.urls')),
     path('admin/', admin.site.urls),
 ]

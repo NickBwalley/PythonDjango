379299825fe0622772dd3981e31ca7f71765b7f1
NickBwalley <nickbwalley@gmail.com>
2021-05-05 17:29:58 +0300

    Python Django

--- ConsoleSystem/ConsoleSystem/urls.py
+++ ConsoleSystem/ConsoleSystem/urls.py
@@ -14,19 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('HomePage')
-
-
-def about(request):
-    return HttpResponse('AboutUs')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', about)
+    path('', include('accounts.urls')),
+   
 ]
--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("HomePage")
+
+
+def products(request):
+    return HttpResponse("ProductPage")
+
+
+def customer(request):
+    return HttpResponse("CustomersPage")
+

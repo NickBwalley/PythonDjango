ff319b050339ac35038b6c9b28dabb2692d1a150
NickBwalley <nickbwalley@gmail.com>
2021-05-06 17:28:29 +0300

    Django ConsoleProject

--- ConsoleSystem/accounts/urls.py
+++ ConsoleSystem/accounts/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
-    path('create_order/', views.create_order, name="create_order"),
+    
+    path('create_order/<str:pk>', views.create_order, name="create_order"),
     path('update_order/<str:pk>/', views.update_order, name="update_order"),
     path('delete_order/<str:pk>/', views.delete_order, name="delete_order"),
 ]

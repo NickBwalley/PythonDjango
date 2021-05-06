08859cad6055a29ed2e8c1d18b88ae84b70b438e
NickBwalley <nickbwalley@gmail.com>
2021-05-06 12:40:25 +0300

    Django ConsoleProject

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -27,6 +27,12 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+    context = {'customer': customer, 'orders': orders, 'orders_count': orders_count}
+
+
+    return render(request, 'accounts/customer.html', context)
 

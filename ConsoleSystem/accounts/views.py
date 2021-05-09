e47bf625666a7869284f750f04808aa86b8eddc7
NickBwalley <nickbwalley@gmail.com>
2021-05-09 14:28:09 +0300

    Django ConsoleProject

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -12,36 +12,41 @@ from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 def register_page(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
-            return redirect('login') 
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login') 
 
-    context = {'form':form} 
-    return render(request, 'accounts/register.html', context)
+        context = {'form':form} 
+        return render(request, 'accounts/register.html', context)
 
 
 def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:    
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Incorrect Username or Password! ')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Incorrect Username or Password! ')
-
-    context = {}
-    return render(request, 'accounts/login.html', context)
+        context = {}
+        return render(request, 'accounts/login.html', context)
 
 
 def logout_user(request):

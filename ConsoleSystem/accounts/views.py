a75b98f71f6a5016d78ef73fca1e2ad5cd9907f0
NickBwalley <nickbwalley@gmail.com>
2021-05-09 13:50:09 +0300

    Django Project

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # create your views here
 from .models import *
@@ -14,13 +16,27 @@ def register_page(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save() 
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login') 
 
     context = {'form':form} 
     return render(request, 'accounts/register.html', context)
 
 
 def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     context = {}
     return render(request, 'accounts/login.html', context)
 

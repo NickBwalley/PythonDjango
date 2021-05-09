a88a4f65f743ee295f9dc1d06fe1923f4fa0fa18
NickBwalley <nickbwalley@gmail.com>
2021-05-09 12:51:37 +0300

    Django Projects

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+# create your views here
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 def register_page(request):
-    context = {}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save() 
+
+    context = {'form':form} 
     return render(request, 'accounts/register.html', context)
 
 

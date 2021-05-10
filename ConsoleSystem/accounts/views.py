2b5cf58b17ec10b4fa73ab876acc244d6c3acdaf
NickBwalley <nickbwalley@gmail.com>
2021-05-10 18:05:51 +0300

    Django ConsoleProject

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -24,6 +24,10 @@ def register_page(request):
 
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
+
 
             messages.success(request, 'Account was created for ' + username)
             return redirect('login') 
@@ -73,9 +77,17 @@ def home(request):
     }    
     return render(request, 'accounts/dashboard.html', context)
 
-
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def user_page(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+
+    context = {'orders': orders, 'total_orders': total_orders, 'delivered':delivered, 'pending': pending}
     return render (request, 'accounts/user.html', context)    
 
 @login_required(login_url='login')

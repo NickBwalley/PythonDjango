43c8d1361135e37fb6abe5961caab2b06b6c8107
NickBwalley <nickbwalley@gmail.com>
2021-05-12 20:12:32 +0300

    DjangoProjects

--- ConsoleSystem/ConsoleSystem/settings.py
+++ ConsoleSystem/ConsoleSystem/settings.py
@@ -128,6 +128,8 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

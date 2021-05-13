c51b2044585d529b39667e77d3826b51b34a1e34
NickBwalley <nickbwalley@gmail.com>
2021-05-13 09:44:06 +0300

    Django Project

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -94,8 +94,14 @@ def user_page(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['customer'])
 def account_settings(request):
-    user = request.user.customer
-    form = CustomerForm(instance=user)
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES,  instance=customer)
+        if form.is_valid():
+            form.save()
+
     context ={'form':form}
     return render(request, 'accounts/account_settings.html', context)
 

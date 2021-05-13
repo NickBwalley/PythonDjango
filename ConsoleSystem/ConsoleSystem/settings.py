f38c4cb585b1bd9041f6d9146d62078145657d59
NickBwalley <nickbwalley@gmail.com>
2021-05-13 14:31:32 +0300

    Django Project

--- ConsoleSystem/ConsoleSystem/settings.py
+++ ConsoleSystem/ConsoleSystem/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- ConsoleSystem/accounts/urls.py
+++ ConsoleSystem/accounts/urls.py
@@ -25,11 +25,11 @@ urlpatterns = [
     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
     name="reset_password"),
 
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),name="password_reset_done"),
     
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_view"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), name="password_reset_confirm"),
 
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_complete"),
 
 ]
 

532ce91e23a5ff6f8c56f9d85140b89e46d5de9b
NickBwalley <nickbwalley@gmail.com>
2021-05-13 13:22:49 +0300

    Django Projects

--- ConsoleSystem/accounts/views.py
+++ ConsoleSystem/accounts/views.py
@@ -22,13 +22,6 @@ def register_page(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(
-                user=user,
-            )
-
-
             messages.success(request, 'Account was created for ' + username)
             return redirect('login') 
 
